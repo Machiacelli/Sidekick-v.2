bbc005350604e62244b8a2ce9c1a62e25f0a2df7
Machiacelli <akaffebtd@hotmail.se>
2025-11-01 15:41:57 +0100

    Update download URLs to 032d95b

--- src/sidekick-modular-clean.user.js
+++ src/sidekick-modular-clean.user.js
@@ -10,8 +10,8 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
-// @downloadURL  https://raw.githubusercontent.com/Machiacelli/Sidekick-v.2/14f412b/src/sidekick-modular-clean.user.js?v=20251031-5
-// @updateURL    https://raw.githubusercontent.com/Machiacelli/Sidekick-v.2/14f412b/src/sidekick-modular-clean.user.js?v=20251031-5
+// @downloadURL  https://raw.githubusercontent.com/Machiacelli/Sidekick-v.2/032d95b/src/sidekick-modular-clean.user.js?v=20251101
+// @updateURL    https://raw.githubusercontent.com/Machiacelli/Sidekick-v.2/032d95b/src/sidekick-modular-clean.user.js?v=20251101
 // @connect      api.lzpt.io
 // @require      https://cdn.jsdelivr.net/gh/Machiacelli/Sidekick-v.2@3781930/src/modules/core.module.js?v=20251029
 // @require      https://cdn.jsdelivr.net/gh/Machiacelli/Sidekick-v.2@3b4dfc4/src/modules/ui.module.js?v=20251031-2
